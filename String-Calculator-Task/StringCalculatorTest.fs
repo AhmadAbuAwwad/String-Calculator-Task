37b3206d69220b714b9bc71f5b20ed1feec431b8
AhmadAbuAwwad <ahmad.h.awwad@gmail.com>
2024-07-10 21:17:38 +0300

    Step 1: Implement basic add method for 0, 1, or 2 numbers

---
+++ String-Calculator-Task/StringCalculatorTest.fs
@@ -0,0 +1,35 @@
+module StringCalculatorTest
+
+open NUnit.Framework
+open StringCalculator
+open StringCalculator.ErrorMessages
+
+[<SetUp>]
+let setUp () = ()
+
+[<Test>]
+let ``testAdd_EmptyString_ReturnsZero`` () =
+    Assert.AreEqual(0, add "")
+
+[<Test>]
+let ``testAdd_SingleNumber_ReturnsNumber`` () =
+    Assert.AreEqual(5, add "5")
+
+[<Test>]
+let ``testAdd_TwoNumbers_ReturnsSum`` () =
+    Assert.AreEqual(300, add "100,200")
+
+[<Test>]
+let ``testAdd_MoreThanTwoNumbers_ThrowsInvalidInputException`` () =
+    let ex = Assert.Throws<InvalidInputException>(fun () -> add "1,2,3" |> ignore)
+    Assert.AreEqual(INVALID_INPUT + "Number of inputs exceeds 2", ex.Message)
+
+[<Test>]
+let ``testAdd_EmptyNumberBetweenCommas_ThrowsInvalidInputException`` () =
+    let ex = Assert.Throws<InvalidInputException>(fun () -> add "1,,2" |> ignore)
+    Assert.AreEqual(INVALID_INPUT + "Consecutive delimiters found", ex.Message)
+
+[<Test>]
+let ``testAdd_NonNumericCharacter_ThrowsInvalidInputException`` () =
+    let ex = Assert.Throws<InvalidInputException>(fun () -> add "1,?" |> ignore)
+    Assert.AreEqual(INVALID_INPUT + "? is not a number", ex.Message)
